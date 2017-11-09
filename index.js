5ec52471143f7939291a444ebe9bc3d24fa9e684
PJ Frias <peejfrias@gmail.com>
2017-11-09 22:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ function BoardMember(name, homeState, training){
  }
 
 
- BoardMembber.prototype.approve = function(){
+ BoardMember.prototype.approve = function(){
    return 'You can do that!'
  }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+ B.doCharity = function(){
+   return "I like to help people."
+ }
+
+ BoardMember.prototype.releasePressStatement = function(){
+   return "You will see great things from Scuber."
+ }
+
+ BoardMember.prototype.sayHi = function(){
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+ }
+}
