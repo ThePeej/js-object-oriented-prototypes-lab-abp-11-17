ce27b66db5b201ec6be9d710e9755d1e0a9bbbf1
PJ Frias <peejfrias@gmail.com>
2017-11-09 22:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function BoardMember(name, homeState, training){
  this.name = name
  this.homeState = homeState
  this.training = training
+}
 
  BoardMember.prototype.veto = function(){
    return 'No, I must disagree'
@@ -22,4 +23,4 @@ function BoardMember(name, homeState, training){
  BoardMember.prototype.sayHi = function(){
    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
  }
-}
+
